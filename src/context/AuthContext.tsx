59e7ad6d7bfc9a682b95631a54d92a310dfda8d1
Minwooh <hsoapdc@gmail.com>
2025-12-09 19:39:49 +0900

    feat : context api로 사용자 정보 관리

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { Auth, getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { app } from "firebaseApp";
+
+interface AuthContextProps {
+    children: ReactNode;
+}
+
+const AuthContext = createContext({
+    user: null as User | null,
+});
+
+export const AuthContextProvider = ({ children }: AuthContextProps) => {
+    const auth = getAuth(app);
+    const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setCurrentUser(user);
+            } else {
+                setCurrentUser(user);
+            }
+        });
+    }, [auth]);
+
+    return (
+        <AuthContext.Provider value={{ user: currentUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
